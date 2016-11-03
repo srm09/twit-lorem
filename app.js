62a16e22c45a291e7f03be528e0cfbe68b5d7aec
Sagar Muchhal <muchhalsagar88@gmail.com>
2016-11-03 16:00:38 -0700

    Added redis functionality

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express')
 var app = express()
+var Redis = require('./redis.js'),
+    rClient = new Redis.Redis()
+
+rClient.init()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(3000, function () {
+  rClient.push('foo', 'bar')
   console.log('Example app listening on port 3000!')
+})
+
+app.get('/para/:number', (req, res) => {
+  return res.send("paras")
+})
+
+app.get('/word/:number', (req, res) => {
+  return res.send("words")
+})
+
+app.get('/letter/:number', (req, res) => {
+  return res.send("letters")
 })
